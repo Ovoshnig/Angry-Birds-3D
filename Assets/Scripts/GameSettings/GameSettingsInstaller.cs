3ed0fa14ef95c99200a32b31ec77a5f20b4cc9bf
Ovoshnig <boost45644@gmail.com>
2025-10-25 10:49:35 +0700

    Import shared package from Space Invaders 3D

---
+++ Assets/Scripts/GameSettings/GameSettingsInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+[Serializable]
+public class GameSettingsInstaller : IInstaller
+{
+    [SerializeField] private GameSettings _gameSettings;
+
+    public void Install(IContainerBuilder builder)
+    {
+        builder.RegisterInstance(_gameSettings.SceneSettings);
+
+        builder.RegisterInstance(_gameSettings.AudioSettings);
+    }
+}
