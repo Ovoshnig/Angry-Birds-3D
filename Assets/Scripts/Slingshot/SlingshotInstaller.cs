d66a09f256762cb55e2d2b7a293c146365118c53
Ovoshnig <boost45644@gmail.com>
2025-12-01 02:00:09 +0700

    Refactor SlingshotShooterView

---
+++ Assets/Scripts/Slingshot/SlingshotInstaller.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+[Serializable]
+public class SlingshotInstaller : IInstaller
+{
+    [SerializeField] private SlingshotShooterView _slingshotShooterView;
+
+    public void Install(IContainerBuilder builder) =>
+        builder.RegisterInstance(_slingshotShooterView);
+}
