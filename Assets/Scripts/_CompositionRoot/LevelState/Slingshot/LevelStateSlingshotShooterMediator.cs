08d7a1dd96affbb43ed8c0f06a273a3e486bfb2e
Ovoshnig <boost45644@gmail.com>
2025-12-01 01:56:37 +0700

    Create LevelState class

---
+++ Assets/Scripts/_CompositionRoot/LevelState/Slingshot/LevelStateSlingshotShooterMediator.cs
@@ -0,0 +1,22 @@
+﻿using R3;
+
+public class LevelStateSlingshotShooterMediator : Mediator
+{
+    private readonly LevelStateLogic _levelStateLogic;
+    private readonly SlingshotShooterView _slingshotShooterView;
+
+    public LevelStateSlingshotShooterMediator(LevelStateLogic levelStateLogic,
+        SlingshotShooterView slingshotShooterView)
+    {
+        _levelStateLogic = levelStateLogic;
+        _slingshotShooterView = slingshotShooterView;
+    }
+
+    public override void Initialize()
+    {
+        _slingshotShooterView.BirdCollided
+            .Subscribe(_ => _levelStateLogic.SetState(LevelState.Idle))
+            .AddTo(CompositeDisposable);
+    }
+}
+
