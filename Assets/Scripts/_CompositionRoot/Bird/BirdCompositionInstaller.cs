4a079124af2dfcc81cc0cb84d9036a2ef006000d
Ovoshnig <boost45644@gmail.com>
2025-12-01 02:03:16 +0700

    Create mediators and installers for composing Bird, Slingshot, and LevelState assemblies

---
+++ Assets/Scripts/_CompositionRoot/Bird/BirdCompositionInstaller.cs
@@ -0,0 +1,9 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+public class BirdCompositionInstaller : IInstaller
+{
+    public void Install(IContainerBuilder builder) =>
+        builder.RegisterEntryPoint<BirdQueueLevelStateMediator>(Lifetime.Singleton);
+}
+
