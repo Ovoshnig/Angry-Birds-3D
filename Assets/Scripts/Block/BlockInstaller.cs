d93340eb9b53c80de2641d0d4c4f7a65b28362e8
Ovoshnig <boost45644@gmail.com>
2025-12-04 12:59:51 +0700

    Create block collision installers

---
+++ Assets/Scripts/Block/BlockInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+[Serializable]
+public class BlockInstaller : IInstaller
+{
+    [SerializeField] private BlockCollisionInstaller _blockCollisionInstaller;
+
+    public void Install(IContainerBuilder builder) =>
+        _blockCollisionInstaller.Install(builder);
+}
---
+++ Assets/Scripts/Block/Collision/BlockCollisionInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+[Serializable]
+public class BlockCollisionInstaller : IInstaller
+{
+    [SerializeField] private GameObject _blockStructure;
+
+    public void Install(IContainerBuilder builder)
+    {
+        builder.Register<BlockCollisionReporter>(Lifetime.Singleton);
+
+        BlockDestructionView[] blockDestructionViews = _blockStructure
+            .GetComponentsInChildren<BlockDestructionView>();
+        builder.RegisterInstance(blockDestructionViews);
+
+        builder.RegisterEntryPoint<BlockCollisionMediator>(Lifetime.Singleton);
+    }
+}
+
