39d4843cd71be0df99aaed46cdbd81bfaabfbf45
Ovoshnig <boost45644@gmail.com>
2025-12-04 15:17:05 +0700

    Create block settings

---
+++ Assets/Scripts/GameSettings/Data/BlockSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class BlockSettings
+{
+    [field: SerializeField, Min(0f)] public float WoodDestructionMultiplier { get; private set; } = 0.02f;
+    [field: SerializeField, Min(0f)] public float StoneDestructionMultiplier { get; private set; } = 0.03f;
+    [field: SerializeField, Min(0f)] public float GlassDestructionMultiplier { get; private set; } = 0.01f;
+}
