93d515536f0eda3cb9ec9b9cdd91032e39260ada
Ovoshnig <boost45644@gmail.com>
2025-12-06 00:14:48 +0700

    Add Destroyed event to BlockDestroyerView

--- Assets/Scripts/Block/Destruction/Views/BlockDestroyerView.cs
+++ Assets/Scripts/Block/Destruction/Views/BlockDestroyerView.cs
@@ -1,3 +1,4 @@
+using R3;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
@@ -6,19 +7,23 @@ public abstract class BlockDestroyerView : MonoBehaviour
 {
     [SerializeField] private GameSettings _gameSettings;
 
+    private readonly Subject<int> _destroyed = new();
+
     private Material _material;
     private CollisionView _collisionView = null;
 
     public CollisionView CollisionView
     {
-        get 
-        { 
+        get
+        {
             if (_collisionView == null)
                 _collisionView = GetComponent<CollisionView>();
 
-            return _collisionView; 
+            return _collisionView;
         }
     }
+
+    public Observable<int> Destroyed => _destroyed;
 
     protected BlockSettings BlockSettings => _gameSettings.BlockSettings;
     protected abstract float DamageMultiplier { get; }
@@ -31,8 +36,13 @@ public abstract class BlockDestroyerView : MonoBehaviour
         _crackAmount += DamageMultiplier * rawDamage;
 
         if (_crackAmount >= 1f)
+        {
+            _destroyed.OnNext(_gameSettings.ScoreSettings.BlockPoints);
             Destroy(gameObject);
+        }
         else
+        {
             _material.SetFloat(BlockDestructionConstants.CrackAmountName, _crackAmount);
+        }
     }
 }
