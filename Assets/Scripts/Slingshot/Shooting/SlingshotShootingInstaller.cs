119dfaa1f610c48c271e60b20d1369455ede9d8b
Ovoshnig <boost45644@gmail.com>
2025-12-07 03:14:33 +0700

    Create slingshot installers

---
+++ Assets/Scripts/Slingshot/Shooting/SlingshotShootingInstaller.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+[Serializable]
+public class SlingshotShootingInstaller : IInstaller
+{
+    [SerializeField] private SlingshotShooterView _slingshotShooterView;
+
+    public void Install(IContainerBuilder builder)
+    {
+        builder.RegisterInstance(_slingshotShooterView);
+
+        builder.RegisterEntryPoint(resolver =>
+        {
+            SlingshotInputHandler slingshotInputHandler = resolver.Resolve<SlingshotInputHandler>();
+            SlingshotSettings slingshotSettings = resolver.Resolve<SlingshotSettings>();
+
+            return new SlingshotShooter(slingshotInputHandler, slingshotSettings,
+                _slingshotShooterView.CenterAnchor,
+                _slingshotShooterView.LeftAnchor.position,
+                _slingshotShooterView.RightAnchor.position,
+                _slingshotShooterView.CenterAnchor.position,
+                _slingshotShooterView.LeftRubber,
+                _slingshotShooterView.RightRubber);
+        }, Lifetime.Singleton).AsSelf();
+    }
+}
