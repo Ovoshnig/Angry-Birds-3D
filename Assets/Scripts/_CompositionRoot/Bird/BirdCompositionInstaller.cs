0d857b9581a034a3f23965247b719437b6082034
Ovoshnig <boost45644@gmail.com>
2025-12-07 13:37:06 +0700

    Implement SFX of bird flying

--- Assets/Scripts/_CompositionRoot/Bird/BirdCompositionInstaller.cs
+++ Assets/Scripts/_CompositionRoot/Bird/BirdCompositionInstaller.cs
@@ -3,7 +3,10 @@ using VContainer.Unity;
 
 public class BirdCompositionInstaller : IInstaller
 {
-    public void Install(IContainerBuilder builder) =>
+    public void Install(IContainerBuilder builder)
+    {
         builder.RegisterEntryPoint<BirdFlyerSlingshotShooterMediator>(Lifetime.Singleton);
+        builder.RegisterEntryPoint<BirdSFXPlayerSlingshotShooterMediator>(Lifetime.Singleton);
+    }
 }
 
