a0b687b20b3efd4a8f18a2e6a6708e317da6ac54
Ovoshnig <boost45644@gmail.com>
2025-12-08 00:22:55 +0700

    Create BlockEntityView, refactor Block classes

--- Assets/Scripts/Block/BlockInstaller.cs
+++ Assets/Scripts/Block/BlockInstaller.cs
@@ -6,12 +6,14 @@ using VContainer.Unity;
 [Serializable]
 public class BlockInstaller : IInstaller
 {
+    [SerializeField] private BlockEntityInstaller _blockEntityInstaller;
     [SerializeField] private BlockCollisionInstaller _blockCollisionInstaller;
     [SerializeField] private BlockDestructionInstaller _blockDestructionInstaller;
     [SerializeField] private BlockSFXPlayingInstaller _blockSFXPlayingInstaller;
 
     public void Install(IContainerBuilder builder)
     {
+        _blockEntityInstaller.Install(builder);
         _blockCollisionInstaller.Install(builder);
         _blockDestructionInstaller.Install(builder);
         _blockSFXPlayingInstaller.Install(builder);
