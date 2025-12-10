49b303f9927ea6efaec34b87f597f64f4c81d36d
Ovoshnig <boost45644@gmail.com>
2025-12-10 23:49:06 +0700

    Rename Score assembly to LevelScore, separate Score and Points

--- Assets/Scripts/LevelScore/Score/ScoreInstaller.cs
+++ Assets/Scripts/LevelScore/Score/ScoreInstaller.cs
@@ -7,17 +7,11 @@ using VContainer.Unity;
 public class ScoreInstaller : IInstaller
 {
     [SerializeField] private ScoreView _scoreView;
-    [SerializeField] private PointsView _pointsViewPrefab;
 
     public void Install(IContainerBuilder builder)
     {
         builder.RegisterComponent(_scoreView);
         builder.Register<ScoreModel>(Lifetime.Singleton);
-        builder.RegisterEntryPoint<ScoreLogic>(Lifetime.Singleton)
-            .AsSelf();
         builder.RegisterEntryPoint<ScoreMediator>(Lifetime.Singleton);
-
-        builder.RegisterInstance(_pointsViewPrefab);
-        builder.Register<PointsObjectPool>(Lifetime.Singleton);
     }
 }
