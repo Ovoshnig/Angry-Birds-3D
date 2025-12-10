72c05d91367be97cc27211af25e8ff74b708f2cd
Ovoshnig <boost45644@gmail.com>
2025-12-10 20:45:17 +0700

    Create PointsObjectPool, implement points show

--- Assets/Scripts/Score/Views/PointsView.cs
+++ Assets/Scripts/Score/Views/PointsView.cs
@@ -1,20 +1,24 @@
 ﻿using DG.Tweening;
+using R3;
 using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(TMP_Text))]
 public class PointsView : MonoBehaviour
 {
     [SerializeField] private TextAnimationStrategy _animationStrategy;
+
+    private readonly ReactiveProperty<bool> _isPlaying = new(false);
 
     private TMP_Text _text;
     private Sequence _sequence;
     private Material _cachedMaterial;
     private Camera _camera;
 
+    public ReadOnlyReactiveProperty<bool> IsPlaying => _isPlaying;
+
     private void Awake()
     {
-        _text = GetComponent<TMP_Text>();
+        _text = GetComponentInChildren<TMP_Text>();
         _camera = Camera.main;
 
         _cachedMaterial = _text.fontMaterial;
@@ -34,7 +38,7 @@ public class PointsView : MonoBehaviour
             Destroy(_cachedMaterial);
     }
 
-    public void Show(Vector3 position)
+    public void Show(Vector3 position, int points)
     {
         if (_animationStrategy == null)
         {
@@ -42,12 +46,23 @@ public class PointsView : MonoBehaviour
             return;
         }
 
-        _sequence?.Kill();
+        if (_sequence != null)
+        {
+            _sequence.onComplete = null;
+            _sequence.onKill = null;
+            _sequence.Kill();
+        }
 
-        transform.parent.position = position;
-        transform.rotation = _camera.transform.rotation;
+        transform.SetPositionAndRotation(position, _camera.transform.rotation);
+        _text.text = points.ToString();
 
         _animationStrategy.ResetState(_text, _cachedMaterial);
         _sequence = _animationStrategy.CreateSequence(_text, _cachedMaterial);
+
+        _isPlaying.Value = true;
+
+        _sequence
+            .OnComplete(() => _isPlaying.Value = false)
+            .OnKill(() => _isPlaying.Value = false);
     }
 }
