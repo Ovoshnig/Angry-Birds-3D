05741a4378dacbe8e4d288be57a47c791e4e13bd
Ovoshnig <boost45644@gmail.com>
2025-12-11 23:47:37 +0700

    Implement camera switching

---
+++ Assets/Scripts/_CompositionRoot/Camera/Slingshot/CameraSwitchSlingshotShooterMediator.cs
@@ -0,0 +1,34 @@
+using R3;
+using System;
+using UnityEngine;
+using static SlingshotShooter;
+
+public class CameraSwitchSlingshotShooterMediator : Mediator
+{
+    private readonly CameraSwitchView _cameraSwitchView;
+    private readonly SlingshotShooter _slingshotShooter;
+
+    public CameraSwitchSlingshotShooterMediator(CameraSwitchView cameraSwitchView,
+        SlingshotShooter slingshotShooter)
+    {
+        _cameraSwitchView = cameraSwitchView;
+        _slingshotShooter = slingshotShooter;
+    }
+
+    public override void Initialize()
+    {
+        _slingshotShooter.CurrentState
+            .Subscribe(SlingshotStateChanged)
+            .AddTo(CompositeDisposable);
+    }
+
+    private void SlingshotStateChanged(SlingshotState state)
+    {
+        if (state == SlingshotState.Idle)
+            _cameraSwitchView.SetPrioritySlingshot();
+        else if (state == SlingshotState.Dragging)
+            _cameraSwitchView.SetPriorityGeneral();
+        else if (state == SlingshotState.Flying)
+            _cameraSwitchView.SetPriorityStructure();
+    }
+}
