715145151a171a9cc2cf9bab208a940553cf55a6
Ovoshnig <boost45644@gmail.com>
2025-12-11 23:47:37 +0700

    Implement camera switching

---
+++ Assets/Scripts/_CompositionRoot/Camera/CameraCompositionInstaller.cs
@@ -0,0 +1,8 @@
+using VContainer;
+using VContainer.Unity;
+
+public class CameraCompositionInstaller : IInstaller
+{
+    public void Install(IContainerBuilder builder) =>
+        builder.RegisterEntryPoint<CameraSwitchSlingshotShooterMediator>(Lifetime.Singleton);
+}
--- Assets/Scripts/_CompositionRoot/CompositionRootInstaller.cs
+++ Assets/Scripts/_CompositionRoot/CompositionRootInstaller.cs
@@ -6,6 +6,7 @@ public class CompositionRootInstaller : IInstaller
     public void Install(IContainerBuilder builder)
     {
         new ScoreCompositionInstaller().Install(builder);
+        new CameraCompositionInstaller().Install(builder);
         new SFXCompositionInstaller().Install(builder);
         new BirdCompositionInstaller().Install(builder);
         new SlingshotCompositionInstaller().Install(builder);
