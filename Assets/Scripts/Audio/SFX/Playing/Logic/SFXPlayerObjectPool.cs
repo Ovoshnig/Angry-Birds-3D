34474489f65ca8a55274235e7afffc9a850d2ad3
Ovoshnig <boost45644@gmail.com>
2025-12-11 14:06:55 +0700

    Create SFXPlayerObjectPool, add IsPlaying in SFXPlayerView

---
+++ Assets/Scripts/Audio/SFX/Playing/Logic/SFXPlayerObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using R3;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Pool;
+using Object = UnityEngine.Object;
+
+public class SFXPlayerObjectPool : IDisposable
+{
+    private readonly ObjectPool<SFXPlayerView> _sfxPlayerPool;
+    private readonly Dictionary<SFXPlayerView, IDisposable> _subscriptions = new();
+
+    public SFXPlayerObjectPool(SFXPlayerView playerPrefab, AudioSettings audioSettings)
+    {
+        _sfxPlayerPool = new ObjectPool<SFXPlayerView>(
+            createFunc: () => Object.Instantiate(playerPrefab),
+            actionOnGet: playerView => playerView.gameObject.SetActive(true),
+            actionOnRelease: OnRelease,
+            defaultCapacity: audioSettings.PoolDefaultCapacity,
+            maxSize: audioSettings.PoolMaxSize
+            );
+    }
+
+    public void Dispose()
+    {
+        foreach (var subscription in _subscriptions.Values)
+            subscription.Dispose();
+
+        _subscriptions.Clear();
+    }
+
+    public void PlaySFX(Vector3 position, AudioResource audioResource)
+    {
+        SFXPlayerView playerView = _sfxPlayerPool.Get();
+        playerView.Play(position, audioResource);
+
+        IDisposable subscription = playerView.IsPlaying
+            .Where(isPlaying => !isPlaying)
+            .Subscribe(_ => _sfxPlayerPool.Release(playerView));
+
+        _subscriptions[playerView] = subscription;
+    }
+
+    private void OnRelease(SFXPlayerView playerView)
+    {
+        _subscriptions[playerView].Dispose();
+        _subscriptions.Remove(playerView);
+        playerView.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Audio/SFX/Playing/Views/SFXPlayerView.cs
+++ Assets/Scripts/Audio/SFX/Playing/Views/SFXPlayerView.cs
@@ -1,12 +1,25 @@
+using R3;
 using UnityEngine;
 using UnityEngine.Audio;
 
 [RequireComponent(typeof(AudioSource))]
 public class SFXPlayerView : MonoBehaviour
 {
+    private readonly ReactiveProperty<bool> _isPlaying = new(false);
+
     private AudioSource _audioSource;
 
-    private void Awake() => _audioSource = GetComponent<AudioSource>();
+    public ReadOnlyReactiveProperty<bool> IsPlaying => _isPlaying;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        Observable
+            .EveryValueChanged(_audioSource, a => a.isPlaying)
+            .Subscribe(isPlaying => _isPlaying.Value = isPlaying)
+            .AddTo(this);
+    }
 
     public void Play(Vector3 position, AudioResource audioResource)
     {
