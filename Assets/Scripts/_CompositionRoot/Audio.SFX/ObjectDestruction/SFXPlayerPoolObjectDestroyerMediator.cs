7dd9a91ad3f7a94b3083189c7c903f95a490bd86
Ovoshnig <boost45644@gmail.com>
2026-04-19 14:10:54 +0700

    Create generic Object Destruction classes, refactor Block Destruction, Pig Destruction, and ObjectDestruction mediators

--- Assets/Scripts/_CompositionRoot/Audio.SFX/ObjectDestruction/SFXPlayerPoolObjectDestroyerMediator.cs
+++ Assets/Scripts/_CompositionRoot/Audio.SFX/ObjectDestruction/SFXPlayerPoolObjectDestroyerMediator.cs
@@ -1,39 +1,40 @@
 using R3;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class SFXPlayerObjectPoolBlockDestroyerMediator : Mediator
+public class SFXPlayerPoolObjectDestroyerMediator<TView> : Mediator where TView : MonoBehaviour
 {
     private readonly SFXPlayerObjectPool _playerObjectPool;
-    private readonly BlockDestroyer _blockDestroyer;
+    private readonly ObjectDestroyer<TView> _destroyer;
 
-    public SFXPlayerObjectPoolBlockDestroyerMediator(SFXPlayerObjectPool playerObjectPool,
-        BlockDestroyer blockDestroyer)
+    public SFXPlayerPoolObjectDestroyerMediator(SFXPlayerObjectPool playerObjectPool,
+        ObjectDestroyer<TView> destroyer)
     {
         _playerObjectPool = playerObjectPool;
-        _blockDestroyer = blockDestroyer;
+        _destroyer = destroyer;
     }
 
     public override void Initialize()
     {
-        _blockDestroyer.Collided.Merge(_blockDestroyer.Damaged)
+        _destroyer.Collided.Merge(_destroyer.Damaged)
             .Where((_, index) => index % 6 == 0)
             .Subscribe(OnDamaged)
             .AddTo(CompositeDisposable);
 
-        _blockDestroyer.Destroyed
+        _destroyer.Destroyed
             .Subscribe(OnDestroyed)
             .AddTo(CompositeDisposable);
     }
 
-    private void OnDamaged(BlockDamageEvent damageEvent)
+    private void OnDamaged(DamageEvent<TView> damageEvent)
     {
         Vector3 position = damageEvent.EntityView.transform.position;
         AudioResource audioResource = damageEvent.AudioResource;
         _playerObjectPool.PlaySFX(position, audioResource);
     }
 
-    private void OnDestroyed(BlockDestructionEvent destructionEvent)
+    private void OnDestroyed(DestructionEvent<TView> destructionEvent)
     {
         Vector3 position = destructionEvent.EntityView.transform.position;
         AudioResource audioResource = destructionEvent.AudioResource;
