dfa191ee48273df1913bd523a71bc70da7cc9870
Ovoshnig <boost45644@gmail.com>
2026-04-20 22:36:45 +0700

    Use SFXPlayerObjectPool for slingshot SFX playing

--- Assets/Scripts/Slingshot/Shooting/Views/SlingshotShooterView.cs
+++ Assets/Scripts/Slingshot/Shooting/Views/SlingshotShooterView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SlingshotShooterView : MonoBehaviour
 {
@@ -7,4 +8,6 @@ public class SlingshotShooterView : MonoBehaviour
     [field: SerializeField] public Transform CenterAnchor { get; private set; }
     [field: SerializeField] public LineRenderer LeftRubber { get; private set; }
     [field: SerializeField] public LineRenderer RightRubber { get; private set; }
+    [field: SerializeField] public AudioClip DraggingResource { get; private set; }
+    [field: SerializeField] public AudioResource ShotResource { get; private set; }
 }
