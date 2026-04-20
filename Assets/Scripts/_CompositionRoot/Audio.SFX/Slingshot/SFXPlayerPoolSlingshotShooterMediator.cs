ce6f71e6c7da266e5f7c6abc8eb0204ebc73a4bf
Ovoshnig <boost45644@gmail.com>
2026-04-20 22:36:45 +0700

    Use SFXPlayerObjectPool for slingshot SFX playing

---
+++ Assets/Scripts/_CompositionRoot/Audio.SFX/Slingshot/SFXPlayerPoolSlingshotShooterMediator.cs
@@ -0,0 +1,31 @@
+﻿using R3;
+using UnityEngine;
+
+public class SFXPlayerPoolSlingshotShooterMediator : Mediator
+{
+    private readonly SFXPlayerObjectPool _playerObjectPool;
+    private readonly SlingshotShooter _shooter;
+    private readonly SlingshotShooterView _shooterView;
+
+    public SFXPlayerPoolSlingshotShooterMediator(SFXPlayerObjectPool playerObjectPool,
+        SlingshotShooter slingshotShooter,
+        SlingshotShooterView slingshotShooterView)
+    {
+        _playerObjectPool = playerObjectPool;
+        _shooter = slingshotShooter;
+        _shooterView = slingshotShooterView;
+    }
+
+    public override void Initialize()
+    {
+        Vector3 position = _shooterView.transform.position;
+
+        _shooter.DraggingStarted
+            .Subscribe(_ => _playerObjectPool.PlaySFX(position, _shooterView.DraggingResource))
+            .AddTo(CompositeDisposable);
+
+        _shooter.Shot
+            .Subscribe(_ => _playerObjectPool.PlaySFX(position, _shooterView.ShotResource))
+            .AddTo(CompositeDisposable);
+    }
+}
