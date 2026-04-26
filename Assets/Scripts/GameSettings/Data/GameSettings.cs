9cf74249f954cbab825e5aa45907f8676056e071
Ovoshnig <boost45644@gmail.com>
2026-04-26 15:28:00 +0700

    Move bird looking from BirdFlyer to BirdFlyerView, remove BirdSettings

--- Assets/Scripts/GameSettings/Data/GameSettings.cs
+++ Assets/Scripts/GameSettings/Data/GameSettings.cs
@@ -8,6 +8,5 @@ public class GameSettings : ScriptableObject
     [field: SerializeField] public AudioSettings AudioSettings { get; private set; }
     [field: SerializeField] public CollisionSettings CollisionSettings { get; private set; }
     [field: SerializeField] public SlingshotSettings SlingshotSettings { get; private set; }
-    [field: SerializeField] public BirdSettings BirdSettings { get; private set; }
     [field: SerializeField] public ScoreSettings ScoreSettings { get; private set; }
 }
