6a3c481bba4385d9a60456f287d156e3378535ee
Ovoshnig <boost45644@gmail.com>
2026-04-26 13:49:47 +0700

    Create SFXPlayerPoolBirdColliderMediator

--- Assets/Scripts/_CompositionRoot/Audio.SFX/SFXMediatorsInstaller.cs
+++ Assets/Scripts/_CompositionRoot/Audio.SFX/SFXMediatorsInstaller.cs
@@ -5,9 +5,11 @@ public class SFXMediatorsInstaller : IInstaller
 {
     public void Install(IContainerBuilder builder)
     {
+        builder.RegisterEntryPoint<SFXPlayerPoolSlingshotShooterMediator>(Lifetime.Singleton);
+
+        builder.RegisterEntryPoint<SFXPlayerPoolBirdColliderMediator>(Lifetime.Singleton);
+
         builder.RegisterEntryPoint<SFXPlayerPoolObjectDestroyerMediator<BlockEntityView>>(Lifetime.Singleton);
         builder.RegisterEntryPoint<SFXPlayerPoolObjectDestroyerMediator<PigEntityView>>(Lifetime.Singleton);
-
-        builder.RegisterEntryPoint<SFXPlayerPoolSlingshotShooterMediator>(Lifetime.Singleton);
     }
 }
