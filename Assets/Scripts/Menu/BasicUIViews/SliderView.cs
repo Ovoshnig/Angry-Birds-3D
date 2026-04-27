38d697a725c8434b06107b72456e9b884bb5b383
Ovoshnig <boost45644@gmail.com>
2026-04-27 22:06:50 +0700

    Create SliderImageView, simplify SliderView

--- Assets/Scripts/Menu/BasicUIViews/SliderView.cs
+++ Assets/Scripts/Menu/BasicUIViews/SliderView.cs
@@ -5,14 +5,13 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Slider))]
 public abstract class SliderView : MonoBehaviour
 {
-    [SerializeField] private Image _fillingImage;
-    [SerializeField] private Sprite[] _fillingSprites;
-
     private readonly ReactiveProperty<float> _value = new();
 
     private Slider _slider = null;
 
     public ReadOnlyReactiveProperty<float> Value => _value;
+    public float MinValue => _slider.minValue;
+    public float MaxValue => _slider.maxValue;
 
     private Slider Slider
     {
@@ -38,30 +37,15 @@ public abstract class SliderView : MonoBehaviour
     {
         Slider.value = value;
         _value.Value = value;
-
-        SetFillingSprite(value);
     }
 
     public void SetValueWithoutNotify(float value)
     {
         Slider.SetValueWithoutNotify(value);
         _value.Value = value;
-
-        SetFillingSprite(value);
     }
 
     public void SetMinValue(float value) => Slider.minValue = value;
 
     public void SetMaxValue(float value) => Slider.maxValue = value;
-
-    private void SetFillingSprite(float value)
-    {
-        if (_fillingImage != null && _fillingSprites.Length > 1)
-        {
-            float percentagePerImage = 1f / (_fillingSprites.Length - 1);
-            float fillingPercentage = (value - _slider.minValue) / (_slider.maxValue - _slider.minValue);
-            int spriteIndex = Mathf.CeilToInt(fillingPercentage / percentagePerImage);
-            _fillingImage.sprite = _fillingSprites[spriteIndex];
-        }
-    }
 }
