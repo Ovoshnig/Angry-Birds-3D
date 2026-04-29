840f79efd9e8594d80509c6f939351e14fe2909a
Ovoshnig <boost45644@gmail.com>
2026-04-29 20:26:19 +0700

    Change the Mediator method for the subscription from Initialize to Start

--- Assets/Scripts/Mediator/Mediator.cs
+++ Assets/Scripts/Mediator/Mediator.cs
@@ -2,11 +2,11 @@ using R3;
 using System;
 using VContainer.Unity;
 
-public abstract class Mediator : IInitializable, IDisposable
+public abstract class Mediator : IStartable, IDisposable
 {
     protected CompositeDisposable CompositeDisposable { get; } = new();
 
-    public abstract void Initialize();
+    public abstract void Start();
 
     public virtual void Dispose() => CompositeDisposable.Dispose();
 }
