638ce65ea052191181fcba74c4f4b4607f56d093
Ovoshnig <boost45644@gmail.com>
2026-05-01 00:33:47 +0700

    Simplify ResumeButtonView.Clicked initialization

---
+++ Assets/Scripts/Window/ResumeButton/Views/ResumeButtonView.cs
@@ -0,0 +1,17 @@
+﻿using R3;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ResumeButtonView : MonoBehaviour
+{
+    private Button _button;
+
+    public Observable<Unit> Clicked { get; private set; }
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        Clicked = _button.OnClickAsObservable();
+    }
+}
