f3f7019467df119641e78c9ba65178b1fdd0ef52
Ovoshnig <boost45644@gmail.com>
2026-05-02 23:44:44 +0700

    Implement ScoreText closing and LevelCompletionPanel opening when level is completed

--- Assets/Scripts/LevelState/Tracking/LevelStateTrackingInstaller.cs
+++ Assets/Scripts/LevelState/Tracking/LevelStateTrackingInstaller.cs
@@ -1,10 +1,17 @@
 using System;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
 [Serializable]
 public class LevelStateTrackingInstaller : IInstaller
 {
-    public void Install(IContainerBuilder builder) =>
+    [SerializeField] private CompletionPanelView _completionPanelView;
+
+    public void Install(IContainerBuilder builder)
+    {
+        builder.RegisterInstance(_completionPanelView);
         builder.Register<LevelStateTracker>(Lifetime.Singleton);
+        builder.RegisterEntryPoint<CompletionViewLevelTrackerMediator>(Lifetime.Singleton);
+    }
 }
