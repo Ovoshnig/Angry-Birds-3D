56fc4feffbb056c7483cde9d6aa0c00c360ae6a8
Ovoshnig <boost45644@gmail.com>
2026-05-15 01:17:06 +0700

    Make DataStorage mediators inherits from UIMediator

--- Assets/Scripts/DataStorage/Storage/Mediators/SaveStorageResetViewMediator.cs
+++ Assets/Scripts/DataStorage/Storage/Mediators/SaveStorageResetViewMediator.cs
@@ -1,18 +1,18 @@
 ﻿using R3;
 
-public class SaveStorageResetViewMediator : Mediator
+public class SaveStorageResetViewMediator : UIMediator<SaveResetButtonView>
 {
     private readonly SaveStorage _saveStorage;
     private readonly SaveResetButtonView _saveResetButtonView;
 
-    public SaveStorageResetViewMediator(SaveStorage saveStorage, 
-        SaveResetButtonView saveResetButtonView)
+    public SaveStorageResetViewMediator(SaveStorage saveStorage,
+        SaveResetButtonView saveResetButtonView) : base(saveResetButtonView)
     {
         _saveStorage = saveStorage;
         _saveResetButtonView = saveResetButtonView;
     }
 
-    public override void Start()
+    protected override void OnViewEnabled()
     {
         _saveResetButtonView.Clicked
             .Subscribe(_ => _saveStorage.ResetData())
--- Assets/Scripts/DataStorage/Storage/Mediators/SettingsStorageResetViewMediator.cs
+++ Assets/Scripts/DataStorage/Storage/Mediators/SettingsStorageResetViewMediator.cs
@@ -1,21 +1,21 @@
 ﻿using R3;
 
-public class SettingsStorageResetViewMediator : Mediator
+public class SettingsStorageResetViewMediator : UIMediator<SettingsResetButtonView>
 {
     private readonly SettingsStorage _settingsStorage;
     private readonly SettingsResetButtonView _settingsResetButtonView;
 
-    public SettingsStorageResetViewMediator(SettingsStorage settingsStorage, 
-        SettingsResetButtonView settingsResetButtonView)
+    public SettingsStorageResetViewMediator(SettingsStorage settingsStorage,
+        SettingsResetButtonView settingsResetButtonView) : base(settingsResetButtonView)
     {
         _settingsStorage = settingsStorage;
         _settingsResetButtonView = settingsResetButtonView;
     }
 
-    public override void Start()
+    protected override void OnViewEnabled()
     {
         _settingsResetButtonView.Clicked
             .Subscribe(_ => _settingsStorage.ResetData())
-            .AddTo(Disposables);
+            .AddTo(ViewDisposables);
     }
 }
