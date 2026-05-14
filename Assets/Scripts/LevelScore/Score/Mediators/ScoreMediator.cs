8cb255681fbc3558324b3c67f130a6f0e023a13a
Ovoshnig <boost45644@gmail.com>
2026-05-15 02:41:33 +0700

    Make ScoreMediator inherits from UIMediator

--- Assets/Scripts/LevelScore/Score/Mediators/ScoreMediator.cs
+++ Assets/Scripts/LevelScore/Score/Mediators/ScoreMediator.cs
@@ -1,17 +1,17 @@
 using R3;
 
-public class ScoreMediator : Mediator
+public class ScoreMediator : UIMediator<ScoreView>
 {
     private readonly ScoreModel _scoreModel;
     private readonly ScoreView _scoreView;
 
-    public ScoreMediator(ScoreModel scoreModel, ScoreView scoreView)
+    public ScoreMediator(ScoreModel scoreModel, ScoreView scoreView) : base(scoreView)
     {
         _scoreModel = scoreModel;
         _scoreView = scoreView;
     }
 
-    public override void Start()
+    protected override void OnViewEnabled()
     {
         _scoreModel.Score
             .Subscribe(_scoreView.SetScoreSmoothly)
