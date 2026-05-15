15c0203dd6a2e215faaad93dba23dd1ef2e58399
Ovoshnig <boost45644@gmail.com>
2026-05-15 21:05:40 +0700

    Make UIView abstract class and its methods protected virtual

--- Assets/Scripts/UI/UI/Views/UIView.cs
+++ Assets/Scripts/UI/UI/Views/UIView.cs
@@ -1,13 +1,13 @@
 using R3;
 using UnityEngine;
 
-public class UIView : MonoBehaviour
+public abstract class UIView : MonoBehaviour
 {
     private readonly ReactiveProperty<bool> _isEnabled = new(false);
 
     public ReadOnlyReactiveProperty<bool> IsEnabled => _isEnabled;
 
-    private void OnEnable() => _isEnabled.Value = true;
+    protected virtual void OnEnable() => _isEnabled.Value = true;
 
-    private void OnDisable() => _isEnabled.Value = false;
+    protected virtual void OnDisable() => _isEnabled.Value = false;
 }
