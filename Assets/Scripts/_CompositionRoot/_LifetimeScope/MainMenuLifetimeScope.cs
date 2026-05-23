cf98734c1dadad67b115bf354f785fac15f26cf8
Ovoshnig <boost45644@gmail.com>
2026-05-23 16:07:46 +0700

    Implement settings reset

--- Assets/Scripts/_CompositionRoot/_LifetimeScope/MainMenuLifetimeScope.cs
+++ Assets/Scripts/_CompositionRoot/_LifetimeScope/MainMenuLifetimeScope.cs
@@ -5,12 +5,14 @@ using VContainer.Unity;
 public class MainMenuLifetimeScope : LifetimeScope
 {
     [SerializeField] private GameQuittingInstaller _gameQuittingInstaller;
+    [SerializeField] private SettingsStorageResetInstaller _settingsStorageResetInstaller;
     [SerializeField] private AudioTuningInstaller _audioTuningInstaller;
     [SerializeField] private ScreenSettingsInstaller _screenSettingsInstaller;
 
     protected override void Configure(IContainerBuilder builder)
     {
         _gameQuittingInstaller.Install(builder);
+        _settingsStorageResetInstaller.Install(builder);
         _audioTuningInstaller.Install(builder);
         _screenSettingsInstaller.Install(builder);
     }
