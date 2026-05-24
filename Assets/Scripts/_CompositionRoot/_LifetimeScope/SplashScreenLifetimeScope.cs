7ae08991494fcaa6f299694896f50b533ca6b6ea
Ovoshnig <boost45644@gmail.com>
2026-05-24 13:47:31 +0700

    Fix SplashScreenDisplayer disposing

--- Assets/Scripts/_CompositionRoot/_LifetimeScope/SplashScreenLifetimeScope.cs
+++ Assets/Scripts/_CompositionRoot/_LifetimeScope/SplashScreenLifetimeScope.cs
@@ -6,6 +6,7 @@ public class SplashScreenLifetimeScope : LifetimeScope
     protected override void Configure(IContainerBuilder builder)
     {
         builder.RegisterEntryPoint<SplashScreenDisplayer>().AsSelf();
-        builder.RegisterEntryPoint<SceneSwitchSplashScreenDisplayerMediator>();
+
+        new SceneMediatorsInstaller().Install(builder);
     }
 }
