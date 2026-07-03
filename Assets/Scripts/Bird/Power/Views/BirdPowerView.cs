46f2b6c7da4a2d862d244177462203c21f3f5731
Ovoshnig <boost45644@gmail.com>
2026-07-03 16:33:55 +0700

    Add an option to check if the bird has a power particle

--- Assets/Scripts/Bird/Power/Views/BirdPowerView.cs
+++ Assets/Scripts/Bird/Power/Views/BirdPowerView.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BirdPowerView : MonoBehaviour
 {
     [field: SerializeField] public BirdPowerType PowerType { get; private set; }
+    [field: SerializeField] public bool HasPowerParticle { get; private set; } = true;
 
     public bool WasActivated { get; private set; } = false;
 
