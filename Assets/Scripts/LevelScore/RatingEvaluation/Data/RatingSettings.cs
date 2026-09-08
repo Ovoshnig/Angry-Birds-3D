9717f5dcb219ccbba0fd544054c8bd988debf404
Ovoshnig <boost45644@gmail.com>
2026-09-08 22:01:28 +0700

    Replace an array with a dictionary in RatingSettings

--- Assets/Scripts/LevelScore/RatingEvaluation/Data/RatingSettings.cs
+++ Assets/Scripts/LevelScore/RatingEvaluation/Data/RatingSettings.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LevelRatingSettings", menuName = "Scriptable Objects/Level Rating Settings")]
 public class RatingSettings : ScriptableObject
 {
+    [SerializeField] private Dictionary<int, int> _levelMaxScores = new();
+
     [field: SerializeField, Min(0)] public int MinStarCount { get; private set; } = 1;
     [field: SerializeField, Min(0)] public int MaxStarCount { get; private set; } = 3;
-    [field: SerializeField] public int[] LevelMaxScoreThresholds { get; private set; }
+
+    public IReadOnlyDictionary<int, int> LevelMaxScores => _levelMaxScores;
 }
